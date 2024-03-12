71370020599beeb13a1035cc03e142a2a4fc5682
Mateusz Wiktorowicz <wktorowicz.mat@gmail.com>
2024-03-12 14:08:36 +0100

    initial commit

---
+++ src/Framework/App.php
@@ -0,0 +1,15 @@
+<?php
+
+#This class is for preparing the framework tools
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class App
+{
+    public function run()
+    {
+        echo "application is running.";
+    }
+}
