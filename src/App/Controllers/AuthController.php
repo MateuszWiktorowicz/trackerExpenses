b65277455628b71b34935ab28cfa24bae21cd8ee
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-03-20 10:14:56 +0100

    UserService add

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService};
 use Framework\TemplateEngine;
 
 class AuthController
 {
     public function __construct(
         private TemplateEngine $view,
-        private ValidatorService $validatorService
+        private ValidatorService $validatorService,
+        private UserService $userService
     ) {
     }
 
