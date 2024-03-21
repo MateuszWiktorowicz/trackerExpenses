ba03617d71493163c146541dd459f8355ff747eb
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-03-21 15:04:58 +0100

    change displaying date format

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -25,7 +25,9 @@ class TransactionService
 
     public function getUserTransaction()
     {
-        $transactions = $this->db->query("SELECT * FROM transactions WHERE user_id = :user_id", [
+        $transactions = $this->db->query("SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date
+        FROM transactions 
+        WHERE user_id = :user_id", [
             'user_id' => $_SESSION['user']
         ])->findAll();
 
