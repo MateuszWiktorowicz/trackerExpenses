ec1562f85eab8d53f642d2496e53f460b149ddd9
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-03-26 08:05:14 +0100

    404 feature

--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -69,6 +69,18 @@ class UserService
     {
         unset($_SESSION['user']);
 
-        session_regenerate_id();
+        //session_regenerate_id();
+
+        $params = session_get_cookie_params();
+
+        setcookie(
+            "PHPSESSID",
+            '',
+            time() - 3600,
+            $params['path'],
+            $params['domain'],
+            $params['secure'],
+            $params['httponly']
+        );
     }
 }
